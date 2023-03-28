25020664cb9e8060a3160e157f0d352602a8c938
Michael Purser <michael.purser@evologics.de>
2023-03-28 18:29:06 +0200

    cmake/Tasks: only build relevant tasks
    - otherwise DUNE crashes with stack smashing error - this should be
      fixed and this commit should then be removed

--- cmake/Tasks.cmake
+++ cmake/Tasks.cmake
@@ -1,5 +1,5 @@
 ############################################################################
-# Copyright 2007-2017 Universidade do Porto - Faculdade de Engenharia      #
+# Copyright 2007-2015 Universidade do Porto - Faculdade de Engenharia      #
 # Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  #
 ############################################################################
 # This file is part of DUNE: Unified Navigation Environment.               #
@@ -8,20 +8,18 @@
 # Licencees holding valid commercial DUNE licences may use this file in    #
 # accordance with the commercial licence agreement provided with the       #
 # Software or, alternatively, in accordance with the terms contained in a  #
-# written agreement between you and Faculdade de Engenharia da             #
-# Universidade do Porto. For licensing terms, conditions, and further      #
-# information contact lsts@fe.up.pt.                                       #
+# written agreement between you and Universidade do Porto. For licensing   #
+# terms, conditions, and further information contact lsts@fe.up.pt.        #
 #                                                                          #
-# Modified European Union Public Licence - EUPL v.1.1 Usage                #
-# Alternatively, this file may be used under the terms of the Modified     #
-# EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md #
+# European Union Public Licence - EUPL v.1.1 Usage                         #
+# Alternatively, this file may be used under the terms of the EUPL,        #
+# Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       #
 # included in the packaging of this file. You may not use this work        #
 # except in compliance with the Licence. Unless required by applicable     #
 # law or agreed to in writing, software distributed under the Licence is   #
 # distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     #
 # ANY KIND, either express or implied. See the Licence for the specific    #
 # language governing permissions and limitations at                        #
-# https://github.com/LSTS/dune/blob/master/LICENCE.md and                  #
 # http://ec.europa.eu/idabc/eupl.html.                                     #
 ############################################################################
 # Author: Ricardo Martins                                                  #
@@ -152,7 +150,13 @@ macro(dune_add_task root_folder task)
 endmacro(dune_add_task root_folder task)
 
 macro(dune_add_tasks root_folder)
-  file(GLOB_RECURSE tasks RELATIVE ${root_folder} ${root_folder}/Task.cmake)
+  # file(GLOB_RECURSE tasks RELATIVE ${root_folder} ${root_folder}/Task.cmake)
+  set (tasks "Sensors/WIC/Task.cmake"
+             "Transports/HTTP/Task.cmake"
+             "Transports/Logging/Task.cmake"
+             "Transports/TCP/Client/Task.cmake"
+             "Transports/UDP/Task.cmake"
+             )
   foreach(task ${tasks})
     dune_add_task(${root_folder} ${task})
   endforeach(task ${tasks})
@@ -171,8 +175,8 @@ if(TASK_FILE)
   endif()
 else(TASK_FILE)
   dune_add_tasks(${PROJECT_SOURCE_DIR}/src)
-  dune_add_tasks(${PROJECT_SOURCE_DIR}/private/src)
-  dune_add_tasks(${PROJECT_SOURCE_DIR}/user/src)
+  # dune_add_tasks(${PROJECT_SOURCE_DIR}/private/src)
+  # dune_add_tasks(${PROJECT_SOURCE_DIR}/user/src)
 endif(TASK_FILE)
 
 list(SORT DUNE_TASKS_ENABLED)
